02c74a76444cb3a59463ed29c14d05fa40dfed1d
rpai21 <pai5@purdue.edu>
2018-10-04 22:19:51 -0400

    Cards updating

--- src/js/components/Rides.jsx
+++ src/js/components/Rides.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import Ride from "./Ride.jsx";
 import firebase from "../firebase.js";
-
+import { Well } from "react-bootstrap";
 class Rides extends Component {
   constructor(props) {
     super(props);
-    this.state = { rides: [] };
+    this.state = { rides: [],
+    chargeType: this.props.chargeType,
+    cost: this.props.cost,
+    date: this.props.date,
+    description: this.props.description,
+    dest: this.props.dest,
+    participants: this.props.participants,
+    paymentMethods: this.props.paymentMethods,
+    picture: this.props.picture,
+    seats: this.props.seats,
+    start: this.props.start,
+    time: this.props.time,
+    trip_id: this.props.trip_id};
   }
 
   componentDidMount() {
@@ -30,7 +42,20 @@ class Rides extends Component {
     return (
       <div>
         {this.state.rides.map(ride => (
-          <Ride key={ride.trip_id} ride={ride} />
+          <Well>
+            <p>Ride chargeType: {}</p>
+            <p>Ride cost: {ride.costs}</p>
+            <p>Ride date {this.state.date}</p>
+            <p>Ride description: {ride.description}</p>
+            <p>Ride dest: {this.state.dest}</p>
+            <p>Ride participants: {this.state.participants}</p>
+            <p>Ride paymentMethods: {this.state.paymentMethods}</p>
+            <p>Ride picture: {this.state.picture}</p>
+            <p>Ride seats: {this.state.seats}</p>
+            <p>Ride start: {this.state.start}</p>
+            <p>Ride time: {this.state.time}</p>
+            <p>Ride trip_id: {this.state.trip_id}</p>
+          </Well>
         ))}
       </div>
     );
