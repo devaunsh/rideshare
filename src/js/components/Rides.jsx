845261a0ca54501e9573bcc500fb769c1d16a0ca
Vineel Guntupalli <guntupal@purdue.edu>
2018-11-01 22:00:45 -0400

    bug fix

--- src/js/components/Rides.jsx
+++ src/js/components/Rides.jsx
@@ -2,16 +2,6 @@ import React, { Component } from "react";
 import Ride from "./Ride.jsx";
 import firebase from "../firebase.js";
 import { Well, Table } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
-} from "reactstrap";
 class Rides extends Component {
   constructor(props) {
     super(props);
@@ -91,23 +81,6 @@ class Rides extends Component {
     return (
       <div className="container-fluid">
         <h2>Available Rides</h2>
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
         <Table striped bordered condensed hover>
           <thead>
             <tr>
